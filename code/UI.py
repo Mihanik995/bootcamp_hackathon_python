2fafae436ca260fe2a6a686f881d051953652670
user <user@example.com>
2025-04-25 17:46:23 +0300

    add 'viewed' parameter changing into UI

--- code/UI.py
+++ code/UI.py
@@ -4,7 +4,8 @@ action_options = """
 1. View movies list
 2. Add new movie
 3. Delete a movie
-4. Restore movies list from backup
+4. Mark movie as viewed
+5. Restore movies list from backup
 
 0. Quit
 """
@@ -49,6 +50,21 @@ def ui() -> bool:
             else:
                 print('No movies found')
         case 4:
+            if Film.all():
+                for film in Film.all():
+                    print(film.__repr__())
+                try:
+                    user_input = int(input('Choose a movie: '))
+                    if user_input not in [film.id for film in Film.all()]:
+                        print('Invalid input')
+                        return True
+                    Film.get(user_input).change_viewed()
+                    print(f'Movie "{Film.get(user_input).title}" marked as viewed')
+                except ValueError:
+                    print('I need a number')
+            else:
+                print('No movies found')
+        case 5:
             Film.load_from_backup()
             print('Movies loaded')
         case _:
--- code/models.py
+++ code/models.py
@@ -4,7 +4,7 @@ from code.utils import execute_query, get_film_by_api
 
 
 class Film:
-    def __init__(self, title: str, description: str, genre: str, year: str, rating: float):
+    def __init__(self, title: str, description: str, genre: str, year: str, rating: float, viewed: bool = False):
         execute_query(
             'create table if not exists films (id serial primary key, title varchar(50), description text, '
             'genre varchar(30), year varchar(15), rating float, viewed bool default false)')
@@ -14,29 +14,31 @@ class Film:
         self.genre = genre
         self.year = year
         self.rating = rating
+        self.viewed = viewed
 
         self.id = self.save()
-        self.viewed = False
 
     def __repr__(self):
-        return f"{self.id}. {self.title}, {self.year} - {self.genre}."
+        return f"{self.id}. {self.title}, {self.year} - {self.genre}. {'Viewed' if self.viewed else 'Not viewed'}"
 
     def __str__(self):
-        return f"{self.id}. {self.title}, {self.year} - {self.genre}.\n{self.description}\nIMDb rating - {self.rating}"
+        return (f"{self.id}. {self.title}, {self.year} - {self.genre}.\n{self.description}\n"
+                f"IMDb rating - {self.rating}\n"
+                f"{'Viewed' if self.viewed else 'Not viewed'}")
 
     @classmethod
     def get(cls, id):
         response = execute_query(f"select * from films where id={id}")
         if response:
-            return cls(response[0][1], response[0][2], response[0][3], response[0][4], response[0][5])
+            return cls(response[0][1], response[0][2], response[0][3], response[0][4], response[0][5], response[0][6])
 
     @classmethod
     def all(cls):
-        response = execute_query(f"select * from films")
+        response = execute_query(f"select * from films order by id")
         if response:
             result = []
             for row in response:
-                result.append(cls(row[1], row[2], row[3], row[4], row[5]))
+                result.append(cls(row[1], row[2], row[3], row[4], row[5], row[6]))
             return result
 
     @classmethod
@@ -49,12 +51,22 @@ class Film:
                           f"values ('{self.title}', '{self.description}', '{self.genre}', '{self.year}', {self.rating})")
         return execute_query(f"select id from films where title = '{self.title}'")[0][0]
 
-    def update(self, title: str, description: str, genre: str, year: str, rating: float, viewed: bool):
+    def update(self,
+               title: str = None,
+               description: str = None,
+               genre: str = None,
+               year: str = None,
+               rating: float = None,
+               viewed: bool = None):
         if not execute_query(f"select * from films where id = {self.id}"):
             self.id = self.save()
         execute_query(f"update films "
-                      f"set title = '{title}', description = '{description}', genre = '{genre}', "
-                      f"year = '{year}', rating = {rating}, viewed={viewed} where item_id = '{self.id}'")
+                      f"set title = '{title if title else self.title}', "
+                      f"description = '{description if description else self.description}', "
+                      f"genre = '{genre if genre else self.genre}', "
+                      f"year = '{year if year else self.year}', "
+                      f"rating = {rating if rating else self.rating}, "
+                      f"viewed = {viewed if viewed else self.viewed} where id = {self.id}")
 
     def delete(self):
         if execute_query(f"select * from films where id = {self.id}"):
@@ -63,8 +75,8 @@ class Film:
     def change_viewed(self):
         if not execute_query(f"select * from films where id = {self.id}"):
             self.save()
-        self.viewed = True if not self.viewed else False
-        self.update(self.title, self.description, self.genre, self.year, self.rating, self.viewed)
+        self.viewed = True
+        self.update(viewed=self.viewed)
 
     @staticmethod
     def create_backup():
